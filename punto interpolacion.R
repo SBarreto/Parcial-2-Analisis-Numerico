badf4e718aa8a3f1a145d184b3d8cd9f955bb1b8
SBarreto <32142955+SBarreto@users.noreply.github.com>
2018-04-18 08:59:45 -0500

    Encontrar polinomio de interpolacion
    Usando funcion de Bessel

---
+++ punto interpolacion.R
@@ -0,0 +1,14 @@
+# Punto interpolación. ----------------------------------------------------
+Bessel=function(x) cos(x*sen(y))
+
+
+besselI()
+
+x=c(0,0.2,0.4)
+y=c(3.59,3.11,3.08)
+
+datx= x[2:5]
+daty= y[2:5]
+
+polyAjuste = poly.calc(datx,daty)
+polyAjuste
